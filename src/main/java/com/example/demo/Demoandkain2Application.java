2cee2083fbc2e4f45b68f113cef2738769630b6c
andkain <andke1462@gmail.com>
2022-10-04 20:00:12 -0500

    agregar archivos

---
+++ src/main/java/com/example/demo/Demoandkain2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoandkain2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoandkain2Application.class, args);
+	}
+
+}
